775a357bb94e81576eaf04e64991475b83f993b9
Mandie Kao <mandiekao@gmail.com>
2024-08-31 22:28:31 +0800

    update static_root in settings.py

--- housing_prediction/settings.py
+++ housing_prediction/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'public' / 'assets'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
